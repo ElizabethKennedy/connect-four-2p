fa725228d150b0b44cb7f01dc60bd967a233b139
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2023-01-15 11:44:11 -0800

    component tests

---
+++ src/components/UI/smallButton/__test__/SmallButtons.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen, waitFor } from '@testing-library/react';
+
+import { SmallButton } from '../SmallButton';
+
+window.resizeTo = function resizeTo(width, height) {
+  Object.assign(this, {
+    innerWidth: width,
+    innerHeight: height,
+    outerWidth: width,
+    outerHeight: height,
+  }).dispatchEvent(new this.Event('resize'));
+};
+
+describe('small button testing', () => {
+  test('should render component', () => {
+    render(<SmallButton>Menu</SmallButton>);
+    const btn = screen.getByRole('button');
+    expect(btn).toBeInTheDocument();
+  });
+});
